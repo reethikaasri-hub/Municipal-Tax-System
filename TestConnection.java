81cce697b0bdc3c84bb08cde1b51c2f1173a58e4
reethikaasri-hub <reethikaareethu@gmail.com>
2026-04-13 21:06:39 +0530

    Add TestConnection class to test database connection

---
+++ TestConnection.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+
+public class TestConnection {
+    public static void main(String[] args) {
+        Connection con = DBConnection.getConnection();
+
+        if (con != null) {
+            System.out.println("Connected successfully!");
+        } else {
+            System.out.println("Connection failed!");
+        }
+    }
+}
